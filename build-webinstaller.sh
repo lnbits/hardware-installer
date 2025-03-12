4e32a68a510cf0ecb18b0ddb53d8073614667751
dni ⚡ <office@dnilabs.com>
2025-03-12 11:24:36 +0100

    fixup!

--- build-webinstaller.sh
+++ build-webinstaller.sh
@@ -16,11 +16,11 @@ mkdir -p $INSTALLER_PATH
 for device in $(jq -r '.devices[]' ./hardware-installer/src/versions.json); do
     for version in $(jq -r '.versions[]' ./hardware-installer/src/versions.json); do
         mkdir -p $INSTALLER_PATH/$device/$version
-        wget $RELEASE/$version/$PROJECT_NAME-$version.bin
-        wget $RELEASE/$version/$PROJECT_NAME-$version.partitions.bin
-        wget $RELEASE/$version/$PROJECT_NAME-$version.bootloader.bin
-        mv $PROJECT_NAME-$version.bin $INSTALLER_PATH/$device/$version/main.bin
-        mv $PROJECT_NAME-$version.partitions.bin $INSTALLER_PATH/$device/$version/partitions.bin
-        mv $PROJECT_NAME-$version.bootloader.bin $INSTALLER_PATH/$device/$version/bootloader.bin
+        wget $RELEASE/$version/$PROJECT_NAME-$version.ino.bin
+        wget $RELEASE/$version/$PROJECT_NAME-$version.ino.partitions.bin
+        wget $RELEASE/$version/$PROJECT_NAME-$version.ino.bootloader.bin
+        mv $PROJECT_NAME-$version.ino.bin $INSTALLER_PATH/$device/$version/main.bin
+        mv $PROJECT_NAME-$version.ino.partitions.bin $INSTALLER_PATH/$device/$version/partitions.bin
+        mv $PROJECT_NAME-$version.ino.bootloader.bin $INSTALLER_PATH/$device/$version/bootloader.bin
     done
 done
