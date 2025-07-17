a50dd59a69cab3cbfa0796327f20c2e86cab25bb
Arc <33088785+arcbtc@users.noreply.github.com>
2025-07-17 07:35:15 +0100

    feat: Adds Headings to Configuration Options (#6)
    Co-authored-by: dni ⚡ <office@dnilabs.com>

--- src/Configurator.jsx
+++ src/Configurator.jsx
@@ -10,19 +10,20 @@ export const Configurator = () => {
 
   const upload = async () => {
     if (esploader().transport.device.writable) {
-      const preparedConfig = JSON.stringify(config, null, 2);
+      // Filter out heading elements before uploading
+      const uploadConfig = config.filter((e) => e.type !== "heading");
+      const preparedConfig = JSON.stringify(uploadConfig, null, 2);
       const writer = esploader().transport.device.writable.getWriter();
-        writer.write(enc.encode(`/file-remove ${configPath}\n`));
-        const lines = preparedConfig.split("\n");
-        for (const line of lines) {
-          writer.write(enc.encode(`/file-append ${configPath} ${line}\n`));
-          await new Promise((resolve) => setTimeout(resolve, 100));
-        }
-        writer.releaseLock();
-        term.writeln("Config upload complete!");
+      writer.write(enc.encode(`/file-remove ${configPath}\n`));
+      const lines = preparedConfig.split("\n");
+      for (const line of lines) {
+        writer.write(enc.encode(`/file-append ${configPath} ${line}\n`));
+        await new Promise((resolve) => setTimeout(resolve, 100));
+      }
+      writer.releaseLock();
+      term.writeln("Config upload complete!");
     }
   };
-
 
   const read = async () => {
     await esploader().transport.write(enc.decode(`/file-read ${configPath}\n`));
@@ -32,7 +33,7 @@ export const Configurator = () => {
     const value = e.currentTarget.value;
     const name = e.currentTarget.name;
     config.forEach((element) => {
-      if (element.name === name) {
+      if (element.name === name && element.type !== "heading") {
         element.value = value;
       }
     });
@@ -48,27 +49,41 @@ export const Configurator = () => {
         <h3>Configure Device</h3>
         <button onClick={() => setShow(!show())}>Show Configuration Options</button>
         <Show when={show()}>
-            <div>
-              <button disabled={running()} onClick={reset}>
-                Reset Device (Start Configuration Mode)
-              </button>
-            </div>
-            <For each={config}>
-              {(element) => (
-                <div class="element">
-                  <label for={element.name}>{element.label}</label> <br />
-                  <input
-                    onChange={updateFormValue}
-                    value={element.value}
-                    id={element.name}
-                    name={element.name}
-                    type={element.type}
-                  />
-                </div>
-              )}
-            </For>
-            <button disabled={running()} onClick={upload}>Upload config</button>
-          </Show>
+          <div>
+            <button disabled={running()} onClick={reset}>
+              Reset Device (Start Configuration Mode)
+            </button>
+          </div>
+          <For each={config}>
+            {(element) => {
+              if (element.type !== "heading") {
+                return (
+                  <div style="padding-left: 5px" class="element">
+                    <label for={element.name}>{element.label}: </label>
+                    <input
+                      onChange={updateFormValue}
+                      value={element.value || ""}
+                      id={element.name}
+                      name={element.name}
+                      type={element.type}
+                    />
+                  </div>
+                );
+              }
+              else {
+                return (
+                  <div style="padding-bottom: 10px;" class="element">
+                    <br />
+                    <strong>{element.label}</strong>
+                  </div>
+                );
+              }
+            }}
+          </For>
+          <button disabled={running()} onClick={upload}>
+            Upload config
+          </button>
+        </Show>
       </Show>
     </div>
   );
